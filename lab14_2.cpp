626bd3172764ceabd5dfa590ba1c458e85b20707
Setthanuwat <aftnick2@gmail.com>
2021-12-27 12:41:01 +0700

    "lap14_2"

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -7,7 +7,7 @@ const int M = 70;
 
 void updateImage(bool [][M],int,int,int);
 
-void showImage(const bool [][M]);
+void showImage( const bool [][M]);
 
 int main()
 {
@@ -20,4 +20,63 @@ int main()
         updateImage(image,s,x,y);
     }while(s != 0 || x != 0 || y != 0);
     return 0;
+}
+
+void updateImage(bool image [N][M],int s,int x,int y)
+{
+    image [x][y] = 1;
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0 ; j < M; j++)
+        {
+            if (sqrt(((pow((i-x),2)+pow((j-y),2)))) <= s-1)
+            {
+                image [i][j] = 1;
+            }
+            
+            
+        }
+    }
+    
+}
+
+void showImage( const bool image [N][M])
+{
+    for(int i = 0; i < 72; i++)
+    {
+       
+        cout << '-';
+        
+    }
+   
+    for(int i = 0; i < N; i++)
+    {
+        cout << "\n";
+        cout << "|";
+        
+        for(int j = 0; j <= M+1; j++)
+        {
+            if(image[i][j] == 0 && j < M)
+            {
+                cout << " ";
+            }
+            if(image[i][j] == 1 && j < M)
+            {
+                cout << "*";
+            }
+            if(j == M+1)
+            {
+                cout << "|";
+            }
+        }
+    }
+     cout << "\n";
+     for(int i = 0; i < 72; i++)
+    {
+       
+       cout <<  "-";
+        
+        
+    }
+      cout << "\n";  
 }
